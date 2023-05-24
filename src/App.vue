20a836b9a5e50e6225c56225e7f3ecc1f33b60b6
Aibek002 <200103346@stu.sdu.edu.kz>
2023-05-25 01:48:36 +0600

    Tour

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="body">
+    <HeaderComp />
+    <ContentsComp />
+  </div>
+</template>
+
+<script>
+import HeaderComp from "./components/HeaderComp.vue"
+import ContentsComp from "./components/ContentComp.vue"
+
+export default {
+  components: {
+    HeaderComp, ContentsComp
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,73 @@
+
+<template>
+  <header>
+    <div class="item"><img src="https://github.com/Aibek002/SapaTime/blob/main/src/img/logotype.png?raw=true" alt="logo"></div>
+    <div class="item"><a href="">Home</a> </div>
+    <div class="item"><a href="">About</a> </div>
+    <div class="item"><a href="">Listing</a> </div>
+    <div class="item"><a href="">Car</a> </div>
+    <div class="item"><a href="">Pages</a></div>
+    <div class="item"><a href="">Contact</a> </div>
+    <div class="item"></div>
+    <div class="item"><a :on-click={{toggleData(Currency)}} href="">EUR</a> </div>
+    <div class="item"><a href=""><img src="https://t4.ftcdn.net/jpg/04/87/76/53/240_F_487765314_EWxEN4PaEjMDGbtep3zpNx2VUPJQAtWG.jpg" class='shopIcon' alt=""></a></div>
+    <div class="item"><a href=""><img src="https://cdn-icons-png.flaticon.com/128/310/310869.png" class='loginIcon' alt=""></a></div>
+  </header>
+  <h1>{{Currency }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      usd: [22,33,55,222,1,3,5,5,6,2,1],
+      euro:[21,2,4,5,3,2,2,4,5,5,1,2,3],
+      Currency:'USD'
+    };
+  },
+  methods: {
+    toggleData(Currency) {
+      if (this.Currency === 'USD') {
+        this.Currency = 'EURO';
+      } else {
+        this.Currency = 'USD';
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+header {
+  display: grid;
+  grid-template-columns: 25% 7% 7% 7% 5% 7% 20% 5% 5% 5%;
+  grid-template-areas: 'item item . . . . . . . . .';
+  height: 70px;
+  border-bottom: 1px solid var(--light-grey-color,#EAEEF3);
+  padding-top: 20px;
+  line-height: 3;
+
+}
+
+header a {
+  color: #232323;
+  text-decoration: none;
+  position: relative;
+  font-weight: 700;
+  font-size: 16px;
+  font-family: dm sans,sans-serif;
+}
+header img{
+  height: 50px;
+  margin: 0;
+}
+.loginIcon{
+  height: 30px;
+  padding-top: 8px;
+}
+.shopIcon{
+  height: 30px;
+  padding-top: 8px;
+
+}
+</style>
